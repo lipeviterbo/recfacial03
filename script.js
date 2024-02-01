a67bad6565a055133a52f05a71b7570580d78e54
lipeviterbo <157051069+lipeviterbo@users.noreply.github.com>
2024-02-01 09:51:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+const cam = document.getElementById('cam')
+        
+console.log("parte 01 - inicial");//debug
+        
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+    faceapi.nets.ageGenderNet.loadFromUri('./models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
+]).then(startVideo)
+
+function startVideo() {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => cam.srcObject = stream,
+      err => console.error(err)
+    )
+  }
+ 
+// const loadLabels = () => {
+//     const labels = ['felipe']
+//     return Promise.all(labels.map(async label => {
+//         const descriptions = []
+//         for (let i = 1; i <= 5; i++) {
+//             const img = await faceapi.fetchImage(`./labels/${label}/${i}.jpg`)
+//             const detections = await faceapi
+//                 .detectSingleFace(img)
+//                 .withFaceLandmarks()
+//                 .withFaceDescriptor()
+//             descriptions.push(detections.descriptor)
+//         }
+//         return new faceapi.LabeledFaceDescriptors(label, descriptions)
+//     }))
+// }
+        
+function loadLabels() {
+    const labels = ["cristiano", "felipe", "messi", "neymar"];
+    return Promise.all(
+      labels.map(async (label) => {
+        const descriptions = [];
+        for (let i = 1; i <= 5; i++) {
+          const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
+          const detections = await faceapi
+            .detectSingleFace(img)
+            .withFaceLandmarks()
+            .withFaceDescriptor();
+          descriptions.push(detections.descriptor);
+        }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      })
+    );
+  }
+  
+console.log("parte 02");//debug
+        
+
+
+
+cam.addEventListener('play', async () => {
+    const canvas = faceapi.createCanvasFromMedia(cam)
+    const canvasSize = {
+        width: cam.width,
+        height: cam.height
+    }
+    const labels = await loadLabels()
+    faceapi.matchDimensions(canvas, canvasSize)
+    document.body.appendChild(canvas)
+    setInterval(async () => {
+        const detections = await faceapi
+            .detectAllFaces(
+                cam,
+                new faceapi.TinyFaceDetectorOptions()
+            )
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender()
+            .withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, canvasSize)
+        const faceMatcher = new faceapi.FaceMatcher(labels, 0.6)
+        const results = resizedDetections.map(d =>
+            faceMatcher.findBestMatch(d.descriptor)
+        )
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        resizedDetections.forEach(detection => {
+            const { age, gender, genderProbability } = detection
+            new faceapi.draw.DrawTextField([
+                `${parseInt(age, 10)} years`,
+                `${gender} (${parseInt(genderProbability * 100, 10)})`
+            ], detection.detection.box.topRight).draw(canvas)
+        })
+        results.forEach((result, index) => {
+            const box = resizedDetections[index].detection.box
+            const { label, distance } = result
+            new faceapi.draw.DrawTextField([
+                `${label} (${parseInt(distance * 100, 10)})`
+            ], box.bottomRight).draw(canvas)
+        })
+    }, 100)
+})
+        console.log("parte 03 - final");//debug
+    
