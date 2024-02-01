00e1fb77a6d2a787ee35593308cc26f8197206cc
lipeviterbo <157051069+lipeviterbo@users.noreply.github.com>
2024-02-01 09:53:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function loadLabels() {
       labels.map(async (label) => {
         const descriptions = [];
         for (let i = 1; i <= 5; i++) {
-          const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
+          const img = await faceapi.fetchImage(`./labels/${label}/${i}.jpg`);
           const detections = await faceapi
             .detectSingleFace(img)
             .withFaceLandmarks()
